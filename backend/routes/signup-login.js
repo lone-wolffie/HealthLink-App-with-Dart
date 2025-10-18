aeaa7dfd08967e4d90a96e2eacc6b1b23834fa3f
lone-wolffie <karenwanjiru09@gmail.com>
2025-10-18 22:15:43 +0300

    Modify signup-login route

--- backend/routes/signup-login.js
+++ backend/routes/signup-login.js
@@ -4,7 +4,9 @@ import { signup, login } from "../controllers/signup-login.js";
 
 const router = express.Router();
 
+// signup route
 router.post("/signup", signup);
+// login route
 router.post("/login", login);
 
 export default router;
