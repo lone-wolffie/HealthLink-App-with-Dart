aec88012c8f788bc44d2397e314639a9305c871a
lone-wolffie <karenwanjiru09@gmail.com>
2025-10-25 03:28:54 +0300

    Add symptoms.dart in lib/models for symptoms checker

---
+++ frontend/lib/models/symptoms.dart
@@ -0,0 +1,42 @@
+class Symptoms {
+  final int id;
+  final int userId;
+  final String symptom;
+  final String severity;
+  final String notes;
+  final String dateRecorded;
+
+  // constructor - required fields
+  Symptoms({
+    required this.id,
+    required this.userId,
+    required this.symptom,
+    required this.severity,
+    required this.notes,
+    required this.dateRecorded,
+  });
+
+  // create symptoms object from JSON data
+  factory Symptoms.fromJson(Map<String, dynamic> json) {
+    return Symptoms(
+      id: json['id'], 
+      userId: json['user_id'], 
+      symptom: json['symptom'], 
+      severity: json['severity'], 
+      notes: json['notes'],
+      dateRecorded: json['created_at'] ?? '',
+    );
+  }
+
+  // convert symptoms object to json
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'user_id': userId,
+      'symptom': symptom,
+      'severity': severity,
+      'notes': notes,
+      'created_at': dateRecorded,
+    };
+  }
+}
