f7bee2bcad06fed371010482cb092d3f3aff3aa1
lone-wolffie <karenwanjiru09@gmail.com>
2025-10-26 03:14:06 +0300

    Add styled_reusable_button.dart in lib/widgets for a reusable styled button

---
+++ frontend/lib/widgets/styled_reusable_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart'; // material design package
+
+class StyledReusableButton extends StatelessWidget { // appearance doesn’t change
+  final String text; // what button displays
+  final VoidCallback onClick; // button clicked function
+  final Color color;
+
+  // constructor
+  const StyledReusableButton({
+    super.key,
+    required this.text,
+    required this.onClick,
+    this.color = Colors.blue,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton( // base material button widget
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color, // blue
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12), // 12px 
+        ),
+        padding: const EdgeInsets.symmetric(
+          vertical: 14,
+          horizontal: 24
+        ),
+      ),
+      onPressed: onClick,
+      child: Text( // what appears inside the button
+        text,
+        style: const TextStyle(
+          fontSize: 16,
+          color: Colors.white,
+        ),
+      ),
+
+    );
+   
+  }
+}
