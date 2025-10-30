216ba1348ebb8057c24083a3e5a23fe64e975962
lone-wolffie <karenwanjiru09@gmail.com>
2025-10-30 13:04:28 +0300

    Modify clinic_card.dart widget in lib/widgets

--- HealthLink_App_Frontend/lib/widgets/clinic_card.dart
+++ HealthLink_App_Frontend/lib/widgets/clinic_card.dart
@@ -4,14 +4,16 @@ class ClinicCard extends StatelessWidget {
   final String name;
   final String address;
   final String phoneNumber;
-  final String services;
-  final String operatingHours;
+  final String email;
+  final List<String> services;
+  final Map<String, String> operatingHours;
 
   const ClinicCard({
     super.key,
     required this.name,
     required this.address,
     required this.phoneNumber,
+    required this.email,
     required this.services,
     required this.operatingHours,
   });
@@ -20,33 +22,48 @@ class ClinicCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.symmetric(
-        vertical: 8, 
-        horizontal: 12
+        vertical: 8,
+        horizontal: 12,
       ),
-
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
       elevation: 3,
       child: Padding(
         padding: const EdgeInsets.all(12),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(name, style: const TextStyle(
-              fontSize: 18, 
-              fontWeight: FontWeight.bold
-            )),
+            Text(
+              name,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
 
             const SizedBox(height: 6),
-            Text(address, style: const TextStyle(color: Colors.black87)),
+            Text(address),
 
             const SizedBox(height: 6),
-            Text('Phone: $phoneNumber', style: const TextStyle(color: Colors.black54)),
+            Text('Phone: $phoneNumber'),
+            Text('Email: $email'),
+            
+            const SizedBox(height: 10),
+            const Text(
+              'Services:',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
 
-            const SizedBox(height: 6),
-            Text('Services: $services', style: const TextStyle(color: Colors.black54)),
+            Text(services.join(", ")),
 
-            const SizedBox(height: 6),
-            Text('Hours: $operatingHours', style: const TextStyle(color: Colors.black54)),
+            const SizedBox(height: 10),
+            const Text(
+              'Operating Hours:',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+
+            ...operatingHours.entries.map((entry) => Text('${entry.key}: ${entry.value}')),
           ],
         ),
       ),
