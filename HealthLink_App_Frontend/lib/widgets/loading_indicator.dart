129d533a8c1cd5e37269e38e1fa34bc32678f349
lone-wolffie <karenwanjiru09@gmail.com>
2025-11-01 09:51:53 +0300

    Modify loading_indicator.dart in lib/widgets

--- HealthLink_App_Frontend/lib/widgets/loading_indicator.dart
+++ HealthLink_App_Frontend/lib/widgets/loading_indicator.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
 
 class LoadingIndicator extends StatelessWidget {
+  final String? message;
+
   // constructor
   const LoadingIndicator({
     super.key,
+    this.message,
 
   });
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: CircularProgressIndicator(), // shows spinner during loading
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const CircularProgressIndicator(), // shows spinner during loading
+          if (message != null) ... [
+            const SizedBox(height: 12),
+            Text(
+              message!,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ],
+      ),
     );
   }
 }
