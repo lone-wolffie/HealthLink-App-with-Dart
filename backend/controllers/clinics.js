73789c61fd21e8d670225a4ed1f2f36d04eea6b7
lone-wolffie <karenwanjiru09@gmail.com>
2025-11-02 06:34:17 +0300

    Modify clinics.js code in controller backend

--- backend/controllers/clinics.js
+++ backend/controllers/clinics.js
@@ -3,7 +3,7 @@ import { db } from "../config/db.js";
 // get all clinics
 export const getAllClinics = async (req, res) => {
     try {
-        await db.query(
+        const result = await db.query(
             "SELECT * FROM clinics ORDER BY name ASC"
         );
         res.status(200).json(result.rows);
@@ -16,14 +16,14 @@ export const getAllClinics = async (req, res) => {
 // add a clinic
 export const addClinic = async (req, res) => {
     try {
-        const { name, address, phonenumber, email } = req.body;
+        const { name, address, phonenumber, email, latitude, longitude, services, operating_hours } = req.body;
         if (!name || !address || !phonenumber || !email) {
             return res.status(400).json({ message: "Clinic name, address, contact and email are required." });
         }
 
         await db.query(
-            "INSERT INTO clinics (name, address, phonenumber, email) VALUES ($1, $2, $3, $4)",
-            [name, address, phonenumber, email]
+            "INSERT INTO clinics (name, address, phonenumber, email, latitude, longitude, services, operating_hours) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+            [name, address, phonenumber, email, latitude, longitude, services, operating_hours]
         );
         res.status(201).json({ message: "Clinic added successfully." });
     } catch (error) {
