695f0f5a7fc00b24644c46f72843c0ecbbe4c9b1
lone-wolffie <karenwanjiru09@gmail.com>
2025-11-02 06:04:15 +0300

    Modify clinics_screen.dart in lib/screens

--- HealthLink_App_Frontend/lib/screens/clinics_screen.dart
+++ HealthLink_App_Frontend/lib/screens/clinics_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+
+import '../models/clinics.dart';
+import '../services/api_service.dart';
+
 import '../widgets/clinic_card.dart';
 import '../widgets/custom_app_bar.dart';
 
@@ -11,76 +15,50 @@ class ClinicsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(title: 'Nearby Clinics'),
-      body: ListView(
-        children: const [
-          ClinicCard(
-            name: 'Kenyatta National Hospital',
-            address: 'Hospital Rd, Upper Hill, Nairobi',
-            phoneNumber: '+254 20 2726300',
-            email: 'knhadmin@knh.or.ke',
-            services: ['Emergency', 'Surgery', 'Pediatrics'],
-            operatingHours: {
-              "Monday": "08:00 - 17:00",
-              "Tuesday": "08:00 - 17:00",
-              "Wednesday": "08:00 - 17:00",
-              "Thursday": "08:00 - 17:00",
-              "Friday": "08:00 - 17:00",
-              "Saturday": "09:00 - 13:00",
-              "Sunday": "Closed"
-            },
-          ),
+      body: FutureBuilder<List<Clinics>> (
+        future: ApiService.getAllClinics(),
+        builder: (context, snapshot) {
+          // loading
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } 
 
-          ClinicCard(
-            name: 'Aga Khan University Hospital',
-            address: '3rd Parklands Ave, Nairobi',
-            phoneNumber: '+254 20 3662000',
-            email: 'akuh.nairobi@aku.edu',
-            services: ['Emergency', 'Cardiology', 'Oncology'],
-            operatingHours: {
-              "Monday": "08:00 - 17:00",
-              "Tuesday": "08:00 - 17:00",
-              "Wednesday": "08:00 - 17:00",
-              "Thursday": "08:00 - 17:00",
-              "Friday": "08:00 - 15:00",
-              "Saturday": "09:00 - 13:00",
-              "Sunday": "Closed"
-            },
-          ),
+          // error
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          }
 
-          ClinicCard(
-            name: 'Nairobi Hospital',
-            address: 'Argwings Kodhek Rd, Nairobi',
-            phoneNumber: '+254 20 2845000',
-            email: 'hosp@nbihosp.org',
-            services: ['Emergency', 'Maternity', 'Surgery'],
-            operatingHours: {
-              "Monday": "08:00 - 18:00",
-              "Tuesday": "08:00 - 18:00",
-              "Wednesday": "08:00 - 18:00",
-              "Thursday": "08:00 - 18:00",
-              "Friday": "08:00 - 18:00",
-              "Saturday": "09:00 - 14:00",
-              "Sunday": "Closed"
-            },
-          ),
+          // empty list retured 
+          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(
+              child: Text('No clinics found'),
+            );
+          }
 
-          ClinicCard(
-            name: 'Gertrudes Children Hospital',
-            address: 'Muthaiga Rd, Nairobi',
-            phoneNumber: '+254 20 7206000',
-            email: 'info@gerties.org',
-            services: ['Pediatrics', 'Emergency', 'Vaccination'],
-            operatingHours: {
-              "Monday": "08:00 - 17:00",
-              "Tuesday": "08:00 - 17:00",
-              "Wednesday": "08:00 - 17:00",
-              "Thursday": "08:00 - 17:00",
-              "Friday": "08:00 - 17:00",
-              "Saturday": "09:00 - 12:00",
-              "Sunday": "Closed"
+          // data found
+          final clinics = snapshot.data!;
+          return ListView.builder(
+            itemCount: clinics.length,
+            itemBuilder: (context, index) {
+              final clinic = clinics[index];
+
+              return ClinicCard(
+                name: clinic.name, 
+                address: clinic.address, 
+                phoneNumber: clinic.phoneNumber, 
+                email: clinic.email, 
+                services: clinic.services, // .split(',').map((service) => service.trim()).toList()
+                operatingHours: clinic.operatingHours.map(
+                  (key, value) => MapEntry(key, value.toString()),
+                ),
+              );
             },
-          ),
-        ],
+          );
+        },
       ),
     );
   }
