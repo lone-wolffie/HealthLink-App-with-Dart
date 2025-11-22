e2aa81d8f5aca208936a9aca6d8f5bc652b14669
lone-wolffie <karenwanjiru09@gmail.com>
2025-11-22 05:19:18 +0300

    Modify cors in backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,9 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: "https://healthlink-frontend.onrender.com"
+}));
 
 // routes usage
 app.use("/uploads/profile", express.static("uploads/profile"));
